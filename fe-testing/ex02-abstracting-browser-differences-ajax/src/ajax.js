c809d7e1c9b6932b7e927997d058a24cf5f37e67
Thomas Junghans <thomas.junghans@gmail.com>
2014-07-25 07:54:18 +0200

    Add: Test from Listing 12.13

--- fe-testing/ex02-abstracting-browser-differences-ajax/src/ajax.js
+++ fe-testing/ex02-abstracting-browser-differences-ajax/src/ajax.js
@@ -1,29 +1,39 @@
 'use strict';
 
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+var tddjs = require("../../tdd");
+
+var create;
+(function() {
+    var xhr;
+    var options = [
+
+        function() {
+            return new ActiveXObject('Microsoft.XMLHTTP');
+        },
+        function() {
+            return new XMLHttpRequest();
+        }
+    ];
+    for (var i = 0, l = options.length; i < l; i++) {
+        try {
+            xhr = options[i]();
+
+            if (typeof xhr.readyState == "number" &&
+                tddjs.isHostMethod(xhr, "open") &&
+                tddjs.isHostMethod(xhr, "send") &&
+                tddjs.isHostMethod(xhr, "setRequestHeader")) {
+
+                create = options[i];
+                break;
+            }
+        } catch (e) {}
+    }
+}());
 
 var ajax = {
-    create: function() {
-        var options = [
-
-            function() {
-                return new ActiveXObject('Microsoft.XMLHTTP');
-            },
-            function() {
-                return new XMLHttpRequest();
-            }
-        ];
-
-        var i;
-        var l = options.length;
-        for (i = 0; i < l; i++) {
-            try {
-                return options[i]();
-            } catch (e) {}
-        }
-
-        return null;
-    }
+    create: create,
+    get: function() {}
 };
 
 module.exports = ajax;
--- fe-testing/ex02-abstracting-browser-differences-ajax/tests/ajax.test.js
+++ fe-testing/ex02-abstracting-browser-differences-ajax/tests/ajax.test.js
@@ -2,12 +2,27 @@
 
 var assert = require("assert");
 var ajax = require("../src/ajax");
+var tddjs = require("../../tdd");
 
 describe('AjaxCreateTest', function() {
     it("test should return XMLHttpRequest object", function() {
         var xhr = ajax.create();
 
         assert.equal(typeof xhr.readyState, 'number');
+        assert.equal(tddjs.isHostMethod(xhr, "open"), true);
+        assert.equal(tddjs.isHostMethod(xhr, "send"), true);
+        assert.equal(tddjs.isHostMethod(xhr, "setRequestHeader"), true);
+    });
+});
+
+describe("GetRequestTest", function() {
+    it("test should define get method", function() {
+        assert.equal(typeof ajax.get, 'function');
     });
 
+    it("test should throw error without url", function() {
+        assert.ifError(function() {
+            ajax.get();
+        }, "TypeError");
+    })
 });
